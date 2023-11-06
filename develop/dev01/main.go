368398e7fab4b5ac0fc3ce13cf240a4d607aed38
potterVombad <be5son0v@yandex.ru>
2023-11-06 19:19:18 +0300

    1-st dev is done

--- develop/dev01/main.go
+++ develop/dev01/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/beevik/ntp"
+)
 
 func main() {
-	fmt.Println("hello")
+	response, err := ntp.Query("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+	err = response.Validate()
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+	time := time.Now().Add(response.ClockOffset).Format("15:04:05")
+	fmt.Println(time)
 }
